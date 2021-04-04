9f490ad119cdc1dacdd7714efb431744793eb5d5
Łukasz Bińkowski <luk.binkowski@gmail.com>
2021-04-05 01:17:01 +0200

    general commit

--- rest-api-cat/src/main/java/src/RestClient.java
+++ rest-api-cat/src/main/java/src/RestClient.java
@@ -1,10 +1,14 @@
 package src;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class RestClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
+
+        InputStreamReader inputStreamReader = new InputStreamReader(url.openStream());
 
     }
 }
