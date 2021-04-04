89f75f0c581cb4298b176c2c9231a1509db416b1
Łukasz Bińkowski <luk.binkowski@gmail.com>
2021-04-05 01:09:48 +0200

    add RestClient class

---
+++ rest-api-cat/src/main/java/src/RestClient.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.net.URL;
+
+public class RestClient {
+    public static void main(String[] args) {
+        URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
+
+    }
+}
